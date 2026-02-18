63dfbe61b38522c461464aa4fc03e3556310b04d
Djalil Ayed <jalilayed@gmail.com>
2026-02-18 16:56:27 +0200

    Create proof.py

---
+++ Love_at_First_Breach_2026_Advanced_Track/St3alMyH34rt/proof.py
@@ -0,0 +1,8 @@
+#tryhackme room St3alMyH34rt 
+# YouTube video walk throgh: https://youtu.be/odYTI5DxFc0
+# Code by Claudi AI
+
+import datetime
+
+with open(r"C:\valentine\static\proof.txt", "w") as f:
+    f.write(f"Code executed at: {datetime.datetime.now()}")
